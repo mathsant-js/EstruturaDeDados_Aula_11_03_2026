ae5ef8c883d8fefd28ebb3afde8a573c66ca5896
mathsant-js <matheus.santana18@fatec.sp.gov.br>
2026-03-11 20:59:10 -0300

    feat: adicionando segundo exemplo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Main {
     public static void main(String[] args) {
+        // Exemplo 1
         Fila fila = new Fila();
         try {
             fila.enfileirar("Matheus");
@@ -23,5 +27,35 @@ public class Main {
             System.out.println(e.getMessage());
         }
         fila.imprime();
+
+        // Exemplo 2
+        Queue<String> waitingQueue = new LinkedList<>();
+
+        waitingQueue.add("Ana");
+        waitingQueue.add("Maria");
+        waitingQueue.add("Ester");
+        waitingQueue.add("Debora");
+
+        System.out.println("Fila: " + waitingQueue);
+
+        // Verifica se a lista está vazia
+        System.out.println("A fila está vazia: " + waitingQueue.isEmpty());
+
+        // Encontra o tamanho da fila
+        System.out.println("Tamanho da fila: " + waitingQueue.size());
+
+        // Verifica se a fila contém um determinado elemento
+        String name = "Ester";
+        if (waitingQueue.contains(name)) {
+            System.out.println("Fila contém " + name);
+        } else {
+            System.out.println("Fila não contém " + name);
+        }
+
+        String firstPersonInTheWaitingQueue = waitingQueue.element();
+        System.out.println("Primeira pessoa da Fila (element()) : " + firstPersonInTheWaitingQueue);
+
+        firstPersonInTheWaitingQueue = waitingQueue.peek();
+        System.out.println("Primeira pessoa da fila: " + firstPersonInTheWaitingQueue);
     }
 }
