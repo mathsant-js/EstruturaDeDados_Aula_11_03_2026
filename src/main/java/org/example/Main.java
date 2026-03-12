99145cea6da6e4988f33e205d9d773e458a424d8
mathsant-js <matheus.santana18@fatec.sp.gov.br>
2026-03-11 21:35:35 -0300

    feat: adicionando os exemplos 3 e 4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -57,5 +58,65 @@ public class Main {
 
         firstPersonInTheWaitingQueue = waitingQueue.peek();
         System.out.println("Primeira pessoa da fila: " + firstPersonInTheWaitingQueue);
+
+        // Exemplo 3
+        Queue<String> fila_de_espera = new LinkedList<>();
+
+        //enfileira
+        fila_de_espera.add("Elias");
+        fila_de_espera.add("Eliseu");
+        fila_de_espera.add("Samuel");
+        fila_de_espera.add("Davi");
+
+        System.out.println("=== Iterações em uma Fila usando Java8 forEach() "
+                + "===");
+        fila_de_espera.forEach(name3 -> {
+            System.out.println(name3);
+        });
+
+        System.out.println("\n=== Iteraçoes em uma Fila usando iterator() ===");
+        Iterator<String> fila_de_esperaIterator = fila_de_espera.iterator();
+        while (fila_de_esperaIterator.hasNext()) {
+            String name3 = fila_de_esperaIterator.next();
+            System.out.println(name);
+        }
+
+        System.out.println("\n=== Iterações em uma Fila usando iterator() e "
+                + "Java8 forEachRemaining() ===");
+        fila_de_esperaIterator = fila_de_espera.iterator();
+        fila_de_esperaIterator.forEachRemaining(name3 -> {
+            System.out.println(name);
+        });
+
+        System.out.println("\n=== Iterações sobre uma Fila usando loop for-each ===");
+        for (String name3 : fila_de_espera) {
+            System.out.println(name);
+        }
+
+        // Exemplo 4
+        Queue<Integer> fila4 = new LinkedList<>();
+
+        // Adiciona os elementos {0, 1, 2, 3, 4} na fila
+        for (int i = 0; i < 5; i++)
+            fila4.add(i);
+
+        // Mostra o conteúdo da fila.
+        System.out.println("Elementos da Fila: " + fila);
+
+        // Remover o cabeça da fila
+        int elemento_removido = fila4.remove();
+        System.out.println("Elemento Removido: " + elemento_removido);
+
+        System.out.println(fila);
+
+        // Visualizar o cabeça da fila
+        int head = fila4.peek();
+        System.out.println("Cabeça da Fila: " + head);
+
+        // Todos os demais métodos da Interface Collection,
+        // tais como size e contains podem ser usados com esta implementação.
+        int tamanho = fila4.size();
+        System.out.println("Tamanho da Fila: " + tamanho);
+
     }
 }
